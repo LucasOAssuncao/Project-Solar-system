65dddd1c5dcbe75307a80bf350821e4a5d7de673
Lucas Otoni <lotoni.vivo@gmail.com>
2022-05-09 16:49:49 -0300

    requisito 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 export default class Missions extends React.Component {
   render() {
-    return <div data-testid="missions"> </div>;
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
   }
 }
