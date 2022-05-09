66f13b3f338e202e5ca32d947830851e42dd4a31
Lucas Otoni <lotoni.vivo@gmail.com>
2022-05-09 16:16:52 -0300

    requisito 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class PlanetCard extends React.Component {
+    render() {
+        const {planetName, planetImage} = this.props;
+      return (
+          <div data-testid="planet-card">
+              <p data-testid="planet-name">{planetName}</p>
+              <img src={planetImage} alt={`Planeta ${planetName}`}></img>
+          </div>
+      )
+    }
+  }
